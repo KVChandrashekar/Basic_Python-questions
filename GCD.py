5044cac9c9e5a6319c0e90209e244a5b83d0b8ef
Vivek Tiwari <35337366+arimanyus@users.noreply.github.com>
2020-10-05 21:47:17 +0530

    Updated GCD to Python3

--- GCD.py
+++ GCD.py
@@ -1,12 +1,12 @@
 //greatest commond divisor
 
 def greatest_common_divisor(x,y):
-    print "For", x, "and", y,","  
+    print("For", x, "and", y,",")
     r=x%y
     while r>0:
         r=x%y
         if r ==0: 
-            print "the greatest common divisor is", y,"."
+            print("the greatest common divisor is", y,".")
         else:
             q=y
             x=q
