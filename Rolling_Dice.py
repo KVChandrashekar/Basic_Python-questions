4219421ec8cc2f50c80ebac3f0e1012536e388ee
Vivek Tiwari <35337366+arimanyus@users.noreply.github.com>
2020-10-05 21:51:08 +0530

    Updated to Python3

--- Rolling_Dice.py
+++ Rolling_Dice.py
@@ -5,9 +5,9 @@ max = 6
 rollSecond = "yes"
 
 while rollSecond == "yes" or rollSecond == "y":
-    print "Rolling the dices..."
-    print "The values are...."
-    print random.randint(min, max)
-    print random.randint(min, max)
+    print("Rolling the dices...")
+    print("The values are....")
+    print(random.randint(min, max))
+    print(random.randint(min, max))
 
     rollSecond = raw_input("Roll the dices again?")
